234bf0659a60508204f3b7ab1f4c623968819526
Jonathan Barton <jonathan.m.barton@gmail.com>
2017-10-15 01:33:56 -0400

    add rest of project

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2014-2017 Cesanta Software Limited
+ * All rights reserved
+ */
+#include "Arduino.h"
+#include "Wire.h"
+#include "Adafruit_BMP085.h"
+
+Adafruit_BMP085 bmp;
+
+void setup(void) {
+  if (!bmp.begin()) {
+	   printf("Can't find a sensor\n");
+	   while (true) delay(1000);
+  }
+}
+
+void loop(void) {
+  delay(2000);
+  printf("Temperature: %.2f *C\n", bmp.readTemperature());
+}
